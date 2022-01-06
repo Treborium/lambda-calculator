a0c61b8bf23a59afcb4ba8107e7573a5cfc6dc21
Robert Fuchs <robert.fuchs@logmein.com>
2022-01-06 21:17:29 +0100

    extract code into function for more readability

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,44 @@
 import {
-  APIGatewayProxyEventV2,
-  APIGatewayProxyStructuredResultV2,
+  APIGatewayProxyEventV2 as ApiGatewayEvent,
+  APIGatewayProxyStructuredResultV2 as ApiGatewayResponse,
 } from "aws-lambda";
-import { stringify } from "querystring";
 import { ErrorMessages } from "./error";
 
 export const handler = async (
-  event: APIGatewayProxyEventV2
-): Promise<APIGatewayProxyStructuredResultV2> => {
+  event: ApiGatewayEvent
+): Promise<ApiGatewayResponse> => {
   console.log("incoming event:", JSON.stringify(event));
   return calculus(event);
 };
 
 export const calculus = async (
-  event: APIGatewayProxyEventV2
-): Promise<APIGatewayProxyStructuredResultV2> => {
+  event: ApiGatewayEvent
+): Promise<ApiGatewayResponse> => {
   if (event.requestContext.http.method !== "GET") {
-    console.log("unsupported http method:", event.requestContext.http.method);
-    return buildResponse(405, {
-      error: true,
-      message: ErrorMessages.UnsupportedHttpMethod,
-    });
+    return unsupportedHttpMethodError(event);
   }
 
   if (!event.queryStringParameters || !event.queryStringParameters["input"]) {
-    console.log(
-      "missing required query parameters. Provided parameters:",
-      JSON.stringify(event.queryStringParameters)
-    );
-
-    return buildResponse(400, {
-      error: true,
-      message: ErrorMessages.MissingRequiredQueryParameter,
-    });
+    return missingRequiredQueryParameterError(event);
   }
 
   const input = event.queryStringParameters["input"];
   const decoded = decodeFromBase64(input);
 
   if (!isArithmeticExpression(decoded)) {
-    console.log("input is not arithmetic expression:", decoded);
-    return buildResponse(400, {
-      error: true,
-      message: ErrorMessages.UnsupportedSymbols,
-    });
+    return unsupportedSymbolsError(decoded);
   }
 
   const result = eval(decoded);
   console.log("input:", decoded, "result:", result);
 
-  return buildResponse(200, { error: false, result})
+  return buildResponse(200, { error: false, result });
 };
 
 const buildResponse = (
   statusCode: number,
   body: LambdaResponseBody
-): APIGatewayProxyStructuredResultV2 => {
+): ApiGatewayResponse => {
   return {
     statusCode,
     body: JSON.stringify(body),
@@ -67,8 +50,42 @@ const decodeFromBase64 = (input: string): string =>
 
 const isArithmeticExpression = (expression: string): boolean => {
   // TODO: add exhaustive unit tests
-  const invalidCharactersRegex = /[^\d\+\-\*/\(\)\s]/gm
+  const invalidCharactersRegex = /[^\d\+\-\*/\(\)\s]/gm;
   return expression.match(invalidCharactersRegex) === null;
+};
+
+const unsupportedHttpMethodError = (
+  event: ApiGatewayEvent
+): ApiGatewayResponse => {
+  console.log("unsupported http method:", event.requestContext.http.method);
+  return buildResponse(405, {
+    error: true,
+    message: ErrorMessages.UnsupportedHttpMethod,
+  });
+};
+
+const missingRequiredQueryParameterError = (
+  event: ApiGatewayEvent
+): ApiGatewayResponse => {
+  console.log(
+    "missing required query parameters. Provided parameters:",
+    JSON.stringify(event.queryStringParameters)
+  );
+
+  return buildResponse(400, {
+    error: true,
+    message: ErrorMessages.MissingRequiredQueryParameter,
+  });
+};
+
+const unsupportedSymbolsError = (
+  input: string
+): ApiGatewayResponse => {
+  console.log("input is not arithmetic expression:", input);
+  return buildResponse(400, {
+    error: true,
+    message: ErrorMessages.UnsupportedSymbols,
+  });
 };
 
 interface LambdaResponseBody {
