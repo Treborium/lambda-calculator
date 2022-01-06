5a5f74437a344b0391d7c71940d5027c5fc6c67a
Robert Fuchs <robert.fuchs@logmein.com>
2022-01-06 15:13:03 +0100

    move index file into src folder

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import {
+  APIGatewayProxyEventV2,
+  APIGatewayProxyStructuredResultV2,
+} from "aws-lambda";
+import { ErrorMessages } from "./error";
+
+export const handler = async (
+  event: APIGatewayProxyEventV2
+): Promise<APIGatewayProxyStructuredResultV2> => {
+  console.log("incoming event:", JSON.stringify(event));
+  return calculus(event);
+};
+
+export const calculus = async (
+  event: APIGatewayProxyEventV2
+): Promise<APIGatewayProxyStructuredResultV2> => {
+  return {
+    statusCode: 200,
+    body: "hello from lambda!",
+  };
+};
+
+const buildResponse = (
+  statusCode: number,
+  body: LambdaResponseBody
+): APIGatewayProxyStructuredResultV2 => {
+  return {
+    statusCode,
+    body: JSON.stringify(body),
+  };
+};
+
+interface LambdaResponseBody {
+  error: boolean;
+  message?: string;
+  result?: number;
+}
